a702c98f3f2c5ea9608166101ed5be5de68447d2
David Van Vleet <56968216+davevanvleet@users.noreply.github.com>
2021-09-22 20:24:58 -0500

    Email changes
    COmmitting changes to ensure email functionality for pythonanywhere.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -127,3 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'davevvwebapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'UNOwebappD3v28$'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
